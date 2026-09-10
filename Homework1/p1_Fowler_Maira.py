7ff38dfa723856e6f2cfa9770a2b5fd8f63860b6
alefowler <mairafowler2024@fau.edu>
2026-09-09 20:40:17 -0400

    Problem 1: Quadratic Equation

---
+++ Homework1/p1_Fowler_Maira.py
@@ -0,0 +1,37 @@
+import math
+import numpy as np  
+import matplotlib.pyplot as plt
+
+while True:
+    user_input= input("Enter a (Enter to exit): ")
+    if user_input == '':
+          break
+
+    a = float(user_input)
+    b = float(input("Enter b: "))
+    c = float(input("Enter c: "))
+ 
+ # discriminant = b^2 - 4ac
+    d = (b**2) - (4*a*c)
+
+    if d > 0:
+            x1 = (-b + (b**2 - 4*a*c)**0.5) / (2*a)
+            x2 = (-b - (b**2 - 4*a*c)**0.5) / (2*a)
+            print(f"two solutions: x1={x1:.5f} and x2={x2:.5f}")
+    elif d == 0:
+            x = -b / (2*a)
+            print(f"one solution: {x:.5f}")
+    else:
+            print("no real solutions")
+
+# plotting quadratic function
+    x = np.linspace(-10, 10, 150)
+    y = a*x**2 + b*x + c
+
+    plt.plot(x, y)
+    plt.title(f'Quadratic Function: {a}x^2 + {b}x + {c}')
+    plt.axhline(0, color='black', lw=0.5, ls='--')
+    plt.axvline(0, color='black', lw=0.5, ls='--')
+    plt.grid()
+    plt.show()
+
